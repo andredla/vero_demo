524c03412acbc5f93312fd1166c44bc8a22f9521
André <andre.dla@gmail.com>
2017-03-30 21:46:59 -0300

    layout

---
+++ vero_demo/www/js/layout.js
@@ -0,0 +1,12 @@
+var layout = {};
+
+layout.nav = "" +
+"<nav id='my-menu' class='mmclasse'>" +
+	"<ul>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"index.html\";});'>Home</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"sincronizar.html\";});'>Sincronizar</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"pedido.html\";});'>Pedido</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"clientes.html\";});'>Clientes</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"foto_documento.html\";});'>Fotografar Documento</a></li>" +
+	"</ul>" +
+"</nav>";
