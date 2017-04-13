33fd0ef2364c17a80042f499851634a9af62824a
André <andre.dla@gmail.com>
2017-04-13 14:05:42 -0300

    novo

--- vero_demo/www/js/layout.js
+++ vero_demo/www/js/layout.js
@@ -5,13 +5,13 @@ layout.nav = "" +
 	"<ul>" +
 		"<li><a href='#' onclick='return mmclose(function(){window.location=\"index.html\";});'>Home</a></li>" +
 		"<li><a href='#' onclick='return mmclose(function(){window.location=\"sincronizar.html\";});'>Sincronizar</a></li>" +
-		"<li><a href='#' onclick='return mmclose(function(){window.location=\"pedido.html\";});'>Pedido</a></li>" +
 		"<li><a href='#' onclick='return mmclose(function(){window.location=\"clientes.html\";});'>Clientes</a></li>" +
-		"<li><a href='#' onclick='return mmclose(function(){window.location=\"foto_documento.html\";});'>Fotografar Documento</a></li>" +
-		"<li><a href='#' onclick='return mmclose(function(){window.location=\"file.html\";});'>File</a></li>" +
-		"<li><a href='#' onclick='return mmclose(function(){window.location=\"include.html\";});'>Include</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"pedido.html\";});'>Pedido</a></li>" +
 		"<li><a href='#' onclick='return mmclose(function(){window.location=\"sign.html\";});'>Assinatura</a></li>" +
-		"<li><a href='#' onclick='return mmclose(function(){window.location=\"geo.html\";});'>GeoLocation</a></li>" +
+		"<li><a href='#' onclick='return mmclose(function(){window.location=\"foto_documento.html\";});'>Documento</a></li>" +
 		"<li><a href='#' onclick='return mmclose(function(){window.location=\"resumo.html\";});'>Resumo</a></li>" +
+		//"<li><a href='#' onclick='return mmclose(function(){window.location=\"file.html\";});'>File</a></li>" +
+		//"<li><a href='#' onclick='return mmclose(function(){window.location=\"include.html\";});'>Include</a></li>" +
+		//"<li><a href='#' onclick='return mmclose(function(){window.location=\"geo.html\";});'>GeoLocation</a></li>" +
 	"</ul>" +
 "</nav>";
